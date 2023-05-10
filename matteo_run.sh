993a03ec165dace2b0119bb20e058f50c8c2f07d
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-05-10 16:13:57 +0200

    lasr sh file

--- matteo_run.sh
+++ matteo_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python main.py --optimizer basinhopping --npoints 100 --layers 1 --absolute --nshots 10000 --nqubits 3 --ndim 3 --target cosnd
+python main.py --optimizer basinhopping --npoints 100 --layers 3 --absolute --nqubits 2 --ndim 2 --target uquark --ansatz qpdf2q
   
